d2b9f8971b01e43e32ed04773869a11d331a6aa7
Yaroslav Boyko <yaroslav.bojko.82@bk.ru>
2023-11-29 19:54:43 +0300

    finish

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int beginHeight = 100;
+    int dailyGrowth = 50;
+    int nightFade = 20;
+
+    int finalHeight = beginHeight + (dailyGrowth - nightFade) * 2 + dailyGrowth / 2;
+
+    std::cout << "Bamboo height by the middle of the third day: " << finalHeight << std::endl;
 }
